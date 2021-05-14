0ee27feb6d944a225144ae1c0f9777071e49a991
shimna <shimnaa.arangil@gmail.com>
2021-05-14 13:30:58 +0530

    draw alphabet E

---
+++ E_Alphabet.py
@@ -0,0 +1,24 @@
+# Draw E alphabet using turtle
+
+import turtle
+
+tt = turtle.Turtle()
+tt.penup()
+tt.setpos(-40,80)
+tt.color("brown")
+tt.pensize(10)
+tt.pendown()
+tt.forward(100)
+tt.backward(100)
+tt.right(90)
+tt.forward(100)
+tt.left(90)
+tt.forward(100)
+tt.backward(100)
+tt.right(90)
+tt.forward(100)
+tt.left(90)
+tt.forward(100)
+
+turtle.done()
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# Turtle" 
+Turtle Programming
+
+1. Square.py - Draw a Square.
+2. Star.py - Draw a Star.
+3. Hexagon.py - Draw a Hexagon.
+4. Polygon.py - Draw a Polygon.
+5. SpiralSquare.py - Draw a spiral square Outside in.
+6. SpiralSquare_inside_out.py - Draw a spiral square inside out.
+7. E_Alphabet.py - Draw the alphabet E.
